575c08f4acdad1a2b23b4148bf8fdac259568338
bizsam <34167131+bizsam@users.noreply.github.com>
2024-02-26 16:25:01 +0530

    Update _hero.blade.php

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -4,17 +4,17 @@
 
   <div class="z-10">
     <h1 class="text-6xl font-bold uppercase text-white">
-      Lara<span class="text-black">Gigs</span>
+      SBLC BG <span class="text-black">Network</span>
     </h1>
     <p class="text-2xl text-gray-200 font-bold my-4">
-      Find or post Laravel jobs & projects
+      Find  SBLC/BG Issuer Beneficiary or Agent
     </p>
     <div>
       @auth
       @else
       <a href="/register"
         class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign
-        Up to List a Gig</a>
+        Up to Post</a>
       @endauth
     </div>
   </div>
